24a3bde56ca036d9cb252c3ededba5394f9c6e6d
Hope <47116705+Hopefuls@users.noreply.github.com>
2020-07-14 02:14:59 +0200

    Update ToggleListener.java
    simpler listener registering

--- src/main/java/com/growlyx/ToggleListener.java
+++ src/main/java/com/growlyx/ToggleListener.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.util.ArrayList;
 
-public class ToggleListener extends JavaPlugin {
+public class ToggleListener extends JavaPlugin implements Listener{
 
     ArrayList<Player> enabled = new ArrayList<>();
 
